334440cf8004255d7f9a88bab7e4835c4f5fd420
HIMANSHU498 <KUSHWAHHIMANSHU498@GMAIL.COM>
2023-05-07 13:07:33 +0530

    sidebar done

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Popup.css";
-const Popup = () => {
+
+const colors = [
+  "#FF79F2",
+  "#43E6FC",
+  "#F19576",
+  "#0047FF",
+  "#6691FF",
+  "#B38BFA",
+  "#17C6AF",
+];
+const Popup = (props) => {
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("");
+  const createBtn = () => {
+    props.cancelPopup({ name, color });
+  };
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  console.log(name);
   return (
-    <>
-      <div className="popup-container">
-        <div className="popup-box">
-          <div className="popup-title">Create New Notes group</div>
-          <label>
-            Group Name
-            <input type="text" placeholder="Enter your group name..." />
-          </label>
-          <div className="icon-colors"> Choose colour</div>
-          <div className="colors"></div>
-          <button className="create-group">Create</button>
+    <div className="popup-container">
+      <div className="popup-box">
+        <div className="popup-title">Create New Notes group</div>
+        <label>
+          Group Name
+          <input
+            type="text"
+            placeholder="Enter your group name..."
+            name="name"
+            onChange={handleChange}
+            value={name}
+          />
+        </label>
+
+        <div className="icon-colors">Choose color</div>
+        <div className="color-box">
+          {colors.map((item, i) => (
+            <div
+              key={i}
+              className="colors"
+              style={{
+                backgroundColor: item,
+                border: `${color.includes(item) ? "3px solid red" : "none"}`,
+              }}
+              onClick={() => setColor(item)}
+            ></div>
+          ))}
         </div>
+
+        <button className="create-group" onClick={createBtn}>
+          Create
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,16 @@ import "./Sidebar.css";
 import Popup from "./Popup";
 const Sidebar = () => {
   const [popup, setPopup] = useState(false);
+  const [groupValues, setGroupValues] = useState([]);
   const CreateBtn = () => {
     setPopup(true);
   };
+
+  const cancelPopup = (userinputs) => {
+    setPopup(false);
+    setGroupValues([...groupValues, userinputs]);
+  };
+
   return (
     <>
       <div className="sidebar-container">
@@ -13,12 +20,24 @@ const Sidebar = () => {
         <button className="create-btn" onClick={CreateBtn}>
           + &nbsp; Create Notes group
         </button>
-        <div className="title-container">
-          <div className="notes-icon">du</div>
-          <div className="notes-title">Cuvette Notes</div>
+        <div className="group-box">
+          {groupValues.map((item) => {
+            return (
+              <div className="title-container">
+                <div className="notes-icon" style={{ background: item.color }}>
+                  {item.name
+                    .split(" ")
+                    .slice(0, 2)
+                    .map((word) => word.charAt(0))
+                    .join("")}
+                </div>
+                <div className="notes-title">{item.name}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
-      {popup && <Popup />}
+      {popup && <Popup cancelPopup={cancelPopup} />}
     </>
   );
 };
